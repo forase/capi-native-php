3f491cad5017274639f41ec053b947c58d540d05
hamzakhomri <hamza.khomri36@gmail.com>
2022-01-08 19:09:02 +0100

    error Class 'Config\Setting' not found

--- facebook/Pageview.php
+++ facebook/Pageview.php
@@ -1,3 +1,41 @@
 <?php
+use Config\Setting;
 
+
+if(!isset($_COOKIE['USERID']) || empty($_COOKIE['USERID'])){
+    $externalId = Setting::getExternalId();
+    setcookie('USERID', $externalId, time()+14*24*3600);
+    $_COOKIE['USERID'] = $externalId;
+}
+
+$access_token = Setting::getAccessToken();
+$pixel_id = Setting::getPixelId();
+
+$api = Api::init(null, null, $access_token);
+$api->setLogger(new CurlLogger());
+$user_data = (new UserData())
+// It is recommended to send Client IP and User Agent for Conversions API Events.
+    ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
+    ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
+    ->setFbc($_COOKIE['_fbc'])
+    ->setExternalId($_COOKIE['USERID'])
+    ->setFbp($_COOKIE['_fbp']);
+
+$eventSourceUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$event = (new Event())
+    ->setEventName('PageView')
+    ->setEventTime(time())
+    ->setEventSourceUrl($eventSourceUrl)
+    ->setUserData($user_data)
+    ->setEventId(FacebookCapiPageView::getEventId())
+    ->setActionSource(ActionSource::WEBSITE);
+
+$events = array();
+array_push($events, $event);
+
+$request = (new EventRequest($pixel_id))
+    ->setTestEventCode(Setting::getTestKey())
+    ->setEvents($events);
+$response = $request->execute();
 ?>
