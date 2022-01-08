df0f3bdf20fdeb847941c0943ba5b92ce9b39707
Bilal Hafri <hafribilal@gmail.com>
2022-01-08 20:04:04 +0100

    comment errors

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 
     require __DIR__ . '/vendor/autoload.php';
     use App\Facebook;
-    include_once 'facebook/Purchase.php';
+    // include_once 'facebook/Purchase.php';
 
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
     if(isset($_POST['submit'])){
         readfile('pages/purchase.html');
-        $purchase = new Purchase();
+        // $purchase = new Purchase();
     }
     else{
         readfile('pages/home.html');
