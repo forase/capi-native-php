c9de33a7b43e30c89a722361bebfb6ae3f7715c9
Bilal Hafri <hafribilal@gmail.com>
2022-01-09 17:32:44 +0100

    fix init Api issue

--- facebook/Purchase.php
+++ facebook/Purchase.php
@@ -10,7 +10,8 @@ use FacebookAds\Object\ServerSide\DeliveryCategory;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\EventRequest;
 use FacebookAds\Object\ServerSide\UserData;
-use App\Config\Setting as Setting;
+use App\Config\Setting;
+
 /**
  *
  */
@@ -23,8 +24,9 @@ class Purchase
   {
     $access_token = Setting::getAccessToken();
     $pixel_id = Setting::getPixelId();
-    echo "token : ".$access_token." ~ pixel id : ".$pixel_id;
-    $this->api = new Api(null, null, $access_token); // ::init
+
+    Api::init(null, null, $access_token);
+    $this->api = Api::instance();
     $this->api->setLogger(new CurlLogger());
 
     $user_data = $this->setUserData($data);
@@ -41,7 +43,7 @@ class Purchase
   }
 
   public function setUserData($data){
-    return (new UserData())
+    $user_data = (new UserData())
     ->setEmails(array('joe@eg.com'))
     ->setPhones(array('12345678901', '14251234567'))
     // It is recommended to send Client IP and User Agent for Conversions API Events.
@@ -49,6 +51,8 @@ class Purchase
     ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
     ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
     ->setFbp('fb.1.1558571054389.1098115397');
+
+    return $user_data;
   }
 
   public function setContent()
