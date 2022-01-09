d40eeb3d2ad1d942115d15d6ff23e5e81c524e8a
Bilal Hafri <hafribilal@gmail.com>
2022-01-09 17:33:02 +0100

    inclide pageview and purchase to index

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 
     require __DIR__ . '/vendor/autoload.php';
     use App\Facebook;
-    // include_once 'facebook/Purchase.php';
+    include_once 'facebook/Purchase.php';
     include_once 'facebook/_Pageview.php';
 
-    
 
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
     if(isset($_POST['submit'])){
         readfile('pages/purchase.html');
-        // $purchase = new Purchase();
+        $data = array();
+        $purchase = new Facebook\Purchase($data);
     }
     else{
         readfile('pages/home.html');
