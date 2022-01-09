e6c045b8a42bba297d09efc549002ef19f1e5261
Bilal Hafri <hafribilal@gmail.com>
2022-01-09 17:32:27 +0100

    fix Api init problem

--- facebook/_Pageview.php
+++ facebook/_Pageview.php
@@ -10,7 +10,8 @@ use FacebookAds\Object\ServerSide\DeliveryCategory;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\EventRequest;
 use FacebookAds\Object\ServerSide\UserData;
-use App\Config\Setting as Setting;
+use App\Config;
+
 /**
  *
  */
@@ -21,50 +22,52 @@ class Pageview
 
   function __construct()
   {
-    echo "hello pageview";
     if(!isset($_COOKIE['USERID']) || empty($_COOKIE['USERID'])){
-        $setting = new Setting();
-        $externalId = Setting::sgetExternalId();
+        $externalId = Config\Setting::setExternalId();
         setcookie('USERID', $externalId, time()+14*24*3600);
         $_COOKIE['USERID'] = $externalId;
     }
 
-    $access_token = Setting::getAccessToken();
-    $pixel_id = Setting::getPixelId();
-    echo "token : ".$access_token." ~ pixel id : ".$pixel_id;
-    $this->api = new Api(null, null, $access_token); // ::init
+    $access_token = Config\Setting::getAccessToken();
+    $pixel_id = Config\Setting::getPixelId();
+    Api::init(null, null, $access_token);
+    $this->api = Api::instance();
     $this->api->setLogger(new CurlLogger());
-
-    $user_data = $this->setUserData($data);
+    echo "token : ".$access_token." ~ pixel id : ".$pixel_id;
+    $user_data = $this->setUserData();
     $eventSourceUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $event = $this->setEvent($user_data,$eventSourceUrl);
 
     array_push($this->events, $event);
 
     $request = (new EventRequest($pixel_id))
-                // ->setTestEventCode(Setting::getTestKey())
+                ->setTestEventCode(Setting::getTestKey())
                 ->setEvents($events);
     $response = $request->execute();
   }
 
-  public function setUserData($data){
+  public function setUserData(){
     return (new UserData())
     // It is recommended to send Client IP and User Agent for Conversions API Events.
     ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
     ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
-    ->setFbc($_COOKIE['_fbc'])
     ->setExternalId($_COOKIE['USERID'])
-    ->setFbp($_COOKIE['_fbp']);
+    ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
+    ->setFbp('fb.1.1558571054389.1098115397');
+    // ->setFbc($_COOKIE['_fbc'])
+    // ->setExternalId($_COOKIE['USERID'])
+    // ->setFbp($_COOKIE['_fbp']);
   }
 
   public function setEvent($user_data,$eventSourceUrl)
   {
+    echo $eventSourceUrl;
     return (new Event())
     ->setEventName('PageView')
     ->setEventTime(time())
     ->setEventSourceUrl($eventSourceUrl)
     ->setUserData($user_data)
-    // ->setEventId(FacebookCapiPageView::getEventId())
+    ->setEventId(Config\Setting::getEventId())
     ->setActionSource(ActionSource::WEBSITE);
   }
 }
