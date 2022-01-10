53d00b67d5e159f74ea00a09643aab0a8589f8da
Bilal Hafri <hafribilal@gmail.com>
2022-01-11 00:54:13 +0100

    GTM setup on progress

--- google/TagManager.php
+++ google/TagManager.php
@@ -1,5 +1,11 @@
 <?php
+namespace App\Google;
 
+use Google\AdsApi\AdManager\AdManagerSession;
+use Google\AdsApi\AdManager\AdManagerSessionBuilder;
+use Google\AdsApi\AdManager\v202111\ApiException;
+use Google\AdsApi\AdManager\v202111\ServiceFactory;
+use Google\AdsApi\Common\OAuth2TokenBuilder;
 /**
  *
  */
@@ -8,7 +14,28 @@ class TagManager
 
   function __construct()
   {
-    // code...
+    // Generate a refreshable OAuth2 credential for authentication.
+    $oAuth2Credential = (new OAuth2TokenBuilder())
+        ->fromFile()
+        ->build();
+    // Construct an API session configured from a properties file and the OAuth2
+    // credentials above.
+    $session = (new AdManagerSessionBuilder())
+        ->fromFile()
+        ->withOAuth2Credential($oAuth2Credential)
+        ->build();
+
+    // Get a service.
+    $serviceFactory = new ServiceFactory();
+    $networkService = $serviceFactory->createNetworkService($session);
+
+    // Make a request
+    $network = $networkService->getCurrentNetwork();
+    printf(
+        "Network with code %d and display name '%s' was found.\n",
+        $network->getNetworkCode(),
+        $network->getDisplayName()
+    );
   }
 }
 
