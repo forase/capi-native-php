d8e7c1e3b3697b85ccf0d971459f898164816c98
Bilal Hafri <hafribilal@gmail.com>
2022-01-11 01:31:08 +0100

    delete Pageview and rename _Pageview to Pageview

--- facebook/Pageview.php
+++ facebook/Pageview.php
@@ -1,42 +1,69 @@
 <?php
-use Config\Setting;
+namespace App\Facebook;
+use FacebookAds\Api;
+use FacebookAds\Logger\CurlLogger;
+use FacebookAds\Object\ServerSide\ActionSource;
+use FacebookAds\Object\ServerSide\Content;
+use FacebookAds\Object\ServerSide\CustomData;
+use FacebookAds\Object\ServerSide\DeliveryCategory;
+use FacebookAds\Object\ServerSide\Event;
+use FacebookAds\Object\ServerSide\EventRequest;
+use FacebookAds\Object\ServerSide\UserData;
+use App\Config\Setting;
+/**
+ *
+ */
+class Pageview
+{
+  private $api = null;
+  private $events = array();
 
-// require_once(realpath(dirname(__FILE__).'/../.config/Setting.php'));
+  function __construct()
+  {
+    if(!isset($_COOKIE['USERID']) || empty($_COOKIE['USERID'])){
+        $externalId = Setting::setExternalId();
+        //setcookie('USERID', $externalId, time()+14*24*3600);
+        $_COOKIE['USERID'] = $externalId;
+    }
 
-if(!isset($_COOKIE['USERID']) || empty($_COOKIE['USERID'])){
-    $externalId = Setting::sgetExternalId();
-    setcookie('USERID', $externalId, time()+14*24*3600);
-    $_COOKIE['USERID'] = $externalId;
-}
+    $access_token = Setting::getAccessToken();
+    $pixel_id = Setting::getPixelId();
+    Api::init(null, null, $access_token,false);
+    $this->api = Api::instance();
+    $this->api->setLogger(new CurlLogger());
+    $user_data = $this->setUserData();
+    $eventSourceUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $event = $this->setEvent($user_data,$eventSourceUrl);
 
-$access_token = Setting::getAccessToken();
-$pixel_id = Setting::getPixelId();
+    array_push($this->events, $event);
 
-$api = Api::init(null, null, $access_token);
-$api->setLogger(new CurlLogger());
-$user_data = (new UserData())
-// It is recommended to send Client IP and User Agent for Conversions API Events.
+    $request = (new EventRequest($pixel_id))
+                ->setTestEventCode(Setting::getTestKey())
+                ->setEvents($this->events);
+    $response = $request->execute();
+  }
+  public function setUserData(){
+    return (new UserData())
+    // It is recommended to send Client IP and User Agent for Conversions API Events.
     ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
     ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
-    ->setFbc($_COOKIE['_fbc'])
-    ->setExternalId($_COOKIE['USERID'])
-    ->setFbp($_COOKIE['_fbp']);
+    // ->setExternalId($_COOKIE['USERID'])
+    ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
+    ->setFbp('fb.1.1558571054389.1098115397');
+    // ->setFbc($_COOKIE['_fbc'])
+    // ->setExternalId($_COOKIE['USERID'])
+    // ->setFbp($_COOKIE['_fbp']);
+  }
 
-$eventSourceUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-$event = (new Event())
+  public function setEvent($user_data,$eventSourceUrl)
+  {
+    return (new Event())
     ->setEventName('PageView')
     ->setEventTime(time())
     ->setEventSourceUrl($eventSourceUrl)
     ->setUserData($user_data)
-    ->setEventId(FacebookCapiPageView::getEventId())
+    ->setEventId(Setting::getEventId())
     ->setActionSource(ActionSource::WEBSITE);
-
-$events = array();
-array_push($events, $event);
-
-$request = (new EventRequest($pixel_id))
-    ->setTestEventCode(Setting::getTestKey())
-    ->setEvents($events);
-$response = $request->execute();
+  }
+}
 ?>
--- index.php
+++ index.php
@@ -2,23 +2,24 @@
     require __DIR__ . '/vendor/autoload.php';
     use App\Facebook;
     include_once 'facebook/Purchase.php';
-    include_once 'facebook/_Pageview.php';
+    include_once 'facebook/Pageview.php';
     error_reporting(E_ALL ^ E_DEPRECATED);
     ini_set('display_errors', 'on');
     if(isset($_POST['submit'])){
         include'pages/purchase.html';
-        $data = array();
-        $purchase = new Facebook\Purchase($data);
+        try{
+            $data = array();
+            $purchase = new Facebook\Purchase($data);
+        }catch (exception $e){
+            echo 'caugt exception', $e->getMessage();
+        }
     }
     else{
         include'pages/home.html';
         try{
-		echo "string";
             $pageview = new Facebook\Pageview();
-		echo "string";
         }catch (exception $e){
             echo 'caugt exception', $e->getMessage();
         }
-
     }
 ?>
